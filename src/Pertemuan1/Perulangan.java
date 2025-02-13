2afe9e47c4a89d0d70c91c6885009b1cb0572aa4
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2025-02-13 12:07:46 +0700

    P1_Percobaan2

---
+++ src/Pertemuan1/Perulangan.java
@@ -0,0 +1,28 @@
+package Pertemuan1;
+
+import java.util.Scanner;
+
+public class Perulangan {
+    public static void main(String[] args) {
+        long nim, n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Nim : ");
+        nim = sc.nextLong();
+        n = nim % 100;
+        if (n < 10) {
+            n += 10;
+        }
+
+        System.out.println("n : " + n);
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print(" *");
+            } else if (i == 6 || i == 10) {
+                continue;
+            } else {
+                System.out.print(" " + i);
+            }
+        }
+
+    }
+}
