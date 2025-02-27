140e26530097c5eaad56534d5bae1579a1715e94
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2025-02-27 08:49:33 +0700

    P3_Percobaan2

--- src/Pertemuan03/Mahasiswa02.java
+++ src/Pertemuan03/Mahasiswa02.java
@@ -5,5 +5,12 @@ public class Mahasiswa02 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    public void cetakInfo() {
+        System.out.println("NIM       : " + this.nim);
+        System.out.println("Nama      : " + this.nama);
+        System.out.println("Kelas     : " + this.kelas);
+        System.out.println("IPK       : " + this.ipk);
+        System.out.println("-----------------------------------");
     }
 }
