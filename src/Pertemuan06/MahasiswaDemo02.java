ca3afc86938c68a728d4c1fd3b52a54eead04893
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2025-03-20 12:44:05 +0700

    P6_Percobaan3

--- src/Pertemuan06/MahasiswaDemo02.java
+++ src/Pertemuan06/MahasiswaDemo02.java
@@ -7,7 +7,7 @@ public class MahasiswaDemo02 {
         Scanner sc = new Scanner(System.in);
         MahasiswaBerprestasi02 list = new MahasiswaBerprestasi02();
         for (int i = 0; i < list.listMhs.length; i++) {
-            System.out.println("Data mahasiswa ke-" + (i + 1) + ": ");
+            System.out.println("Masukkan Data mahasiswa ke-" + (i + 1));
             System.out.print("NIM       : ");
             String nim = sc.nextLine();
             System.out.print("Nama      : ");
@@ -18,6 +18,7 @@ public class MahasiswaDemo02 {
             double ipk = Double.parseDouble(sc.nextLine());
             Mahasiswa02 mhs = new Mahasiswa02(nim, nama, kelas, ipk);
             list.tambah(mhs);
+            System.out.println("-------------------------------");
         }
 
         System.out.println("Data mahasiswa sebelum sorting: ");
@@ -25,6 +26,10 @@ public class MahasiswaDemo02 {
 
         System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
         list.bubbleSort();
+        list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan SELECTOIN SORT (ASC) : ");
+        list.selectionSort();
         list.tampil();
 
         sc.close();
