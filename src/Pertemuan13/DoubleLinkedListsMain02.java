0983622e83e0dea3d5bc26eab1223ba2171fabd1
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2025-05-22 08:29:04 +0700

    P13_Percobaan1

---
+++ src/Pertemuan13/DoubleLinkedListsMain02.java
@@ -0,0 +1,24 @@
+package Pertemuan13;
+
+public class DoubleLinkedListsMain02 {
+    public static void main(String[] args) throws Exception {
+        DoubleLinkedLists02 dll = new DoubleLinkedLists02();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=======================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=======================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=======================");
+        dll.clear();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=======================");
+    }
+}
